e05a061c7b3992bbd1bed73ab3850eb5897f530a
agabybarrios <agabybarrios@gmail.com>
2017-03-23 22:36:16 -0600

    Creación de formulario y modelo para guardar info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def x
   end
+
+  def save_user
+    User.create(name: params[:name], email: params[:email], age: params[:age])
+    redirect_to pages_x_path, notice: 'El usuario se ha creado exitosamente'
+  end
+
 end
